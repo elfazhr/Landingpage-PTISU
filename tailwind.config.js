5a5437a7d6bfb578e2e3e46f616324dce80a3c83
elfazhr <elfa20si@mahasiswa.pcr.ac.id>
2024-02-14 16:05:50 +0700

    created by Elfa

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#B19604',
+      },
+      borderWidth: {
+        '7': '7px',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
